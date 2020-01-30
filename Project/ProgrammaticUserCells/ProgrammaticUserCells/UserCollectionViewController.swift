43ba65e21498ed4f4cba51bed2cb75cebd92d564
Casandra <casandragrullon@pursuit.org>
2020-01-30 09:15:13 -0500

    populated collection view cells with user data

--- Project/ProgrammaticUserCells/ProgrammaticUserCells/UserCollectionViewController.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/UserCollectionViewController.swift
@@ -19,11 +19,39 @@ class UserCollectionViewController: UIViewController {
         userView.collectionView.delegate = self
         userView.collectionView.dataSource = self
         userView.collectionView.register(UINib(nibName: "UserCell", bundle: nil), forCellWithReuseIdentifier: "userCell")
+        getUsers()
+    }
+    
+    private func getUsers() {
+        UsersFetchingService.manager.getUsers { [weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print(appError)
+            case .success(let users):
+                self?.users = users
+            }
+        }
     }
     
 }
 
 extension UserCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+        let maxSize: CGFloat = UIScreen.main.bounds.size.width
+        let numberOfItems: CGFloat = 3
+        //let itemWidth: CGFloat = maxSize.width * 0.40
+        let totalSpace: CGFloat = numberOfItems * itemSpacing
+        let itemWidth: CGFloat = (maxSize - totalSpace) / numberOfItems
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
     
 }
 extension UserCollectionViewController: UICollectionViewDataSource {
